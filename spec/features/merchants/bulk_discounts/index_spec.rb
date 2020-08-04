76f2d5cc4e7348201fbb9edf7100e70c17a8fc1f
Judith Pillado <judith.pillado@gmail.com>
2020-08-04 18:10:02 -0500

    Add link to create a new discount

--- spec/features/merchants/bulk_discounts/index_spec.rb
+++ spec/features/merchants/bulk_discounts/index_spec.rb
@@ -22,12 +22,17 @@ RSpec.describe 'As a Merchant Employee' do
     @discount2 = @merchant2.discounts.create!(discount_percentage: 50, minimum_quantity: 30, merchant: @merchant2)
   end
 
-  it "displays all of the discounts" do
-
+  it "I see all of the discounts" do
     visit "/merchant/discounts"
 
     expect(page).to have_content("Discount: #{@discount1.discount_percentage}%")
     expect(page).to have_content("Minimum Quantity: #{@discount1.minimum_quantity} items")
   end
 
+  it "I see a link to add a new discount " do
+    visit "/merchant/discounts"
+
+    click_link "Create a New Discount"
+    expect(current_path).to eq("/merchant/discounts/new")
+  end
 end
