d636925e5a93119153c7941f183890bd74bd5558
Judith Pillado <judith.pillado@gmail.com>
2020-08-03 19:00:55 -0500

    Create discount model test

---
+++ spec/models/discount_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe Discount do
+  describe 'Relationships' do
+    it {should belong_to :merchant}
+  end
+
+  describe 'Validations' do
+    it {should validate_presence_of :discount_percentage}
+    it {should validate_presence_of :minimum_quantity}
+  end
+
+end
