ae4df35c96db0144f9eca1eeb1013fdeeb9c5d1e
Judith Pillado <judith.pillado@gmail.com>
2020-08-04 20:33:19 -0500

    Add test for new discount form

--- spec/features/merchants/bulk_discounts/new_spec.rb
+++ spec/features/merchants/bulk_discounts/new_spec.rb
@@ -4,32 +4,28 @@ RSpec.describe 'As a Merchant Employee' do
 
   before :each do
     @merchant = create(:merchant)
-    @merchant2 = create(:merchant)
 
     @merchant_employee = create(:user, role: 1, merchant_id: @merchant.id)
 
     @item1 = create(:item, merchant_id: @merchant.id)
     @item2 = create(:item, merchant_id: @merchant.id)
 
-    @item3 = create(:item, merchant_id: @merchant2.id)
-    @item4 = create(:item, merchant_id: @merchant2.id)
-
-    @discount1 = @merchant.discounts.create!(discount_percentage: 20, minimum_quantity: 20, merchant: @merchant1)
-    @discount2 = @merchant2.discounts.create!(discount_percentage: 50, minimum_quantity: 30, merchant: @merchant2)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@merchant_employee)
   end
 
   it "I can create a new discount " do
     visit "/merchant/discounts/new"
-
-    fill_in :discount_percentage, with: 30
-    fill_in :minimum_quantity, with: 10
+    discount_percentage = 30
+    minimum_quantity = 10
+    fill_in :discount_percentage, with: discount_percentage
+    fill_in :minimum_quantity, with: minimum_quantity
 
     click_button 'Create Discount'
 
     expect(current_path).to eq("/merchant/discounts")
 
-    expect(page).to have_content("Discount: 30%")
-    expect(page).to have_content("Minimum Quantity: 5 items")
+    expect(page).to have_content("Discount: #{discount_percentage}%")
+    expect(page).to have_content("Minimum Quantity: #{minimum_quantity} items")
   end
 
 end
