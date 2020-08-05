940e401599326451318705ccdfb0084801259624
Judith Pillado <judith.pillado@gmail.com>
2020-08-04 22:48:55 -0500

    Add route to show view

--- config/routes.rb
+++ config/routes.rb
@@ -71,7 +71,9 @@ Rails.application.routes.draw do
     post "/items", to: "items#create"
     get "/items/:item_id/edit", to: "items#edit"
     patch "/items/:item_id", to: "items#update"
+
     get "/discounts", to: "discounts#index"
+    get "/discounts/:id", to: "discounts#show"
     get "/discounts/new", to: "discounts#new"
     post "/discounts", to: "discounts#create"
     #resources :items
