576f3827cde2a795cd0299d97a34cddf71c47db4
Judith Pillado <judith.pillado@gmail.com>
2020-08-04 20:17:16 -0500

    Add discount id to discount index page

--- spec/features/merchants/bulk_discounts/index_spec.rb
+++ spec/features/merchants/bulk_discounts/index_spec.rb
@@ -4,27 +4,18 @@ RSpec.describe 'As a Merchant Employee' do
 
   before :each do
     @merchant = create(:merchant)
-    @merchant2 = create(:merchant)
 
     @merchant_employee = create(:user, role: 1, merchant_id: @merchant.id)
-    # visit '/login'
-    # fill_in :email, with: @merchant_employee.email
-    # fill_in :password, with: @merchant_employee.password
-    # click_button 'Log In'
-
-    @item1 = create(:item, merchant_id: @merchant.id)
-    @item2 = create(:item, merchant_id: @merchant.id)
-
-    @item3 = create(:item, merchant_id: @merchant2.id)
-    @item4 = create(:item, merchant_id: @merchant2.id)
 
     @discount1 = @merchant.discounts.create!(discount_percentage: 20, minimum_quantity: 20, merchant: @merchant1)
-    @discount2 = @merchant2.discounts.create!(discount_percentage: 50, minimum_quantity: 30, merchant: @merchant2)
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@merchant_employee)
   end
 
   it "I see all of the discounts" do
     visit "/merchant/discounts"
 
+    expect(page).to have_content("Discount ##{@discount1.id}")
     expect(page).to have_content("Discount: #{@discount1.discount_percentage}%")
     expect(page).to have_content("Minimum Quantity: #{@discount1.minimum_quantity} items")
   end
