c68bab8e0f5a7713ad1ab6d2c4f5268b1d241fd8
Judith Pillado <judith.pillado@gmail.com>
2020-08-05 00:03:48 -0500

    Add test to edit discount

--- spec/features/merchants/bulk_discounts/edit_spec.rb
+++ spec/features/merchants/bulk_discounts/edit_spec.rb
@@ -10,11 +10,23 @@ RSpec.describe 'As a Merchant Employee' do
     @item1 = create(:item, merchant_id: @merchant.id)
     @item2 = create(:item, merchant_id: @merchant.id)
 
+    @discount1 = @merchant.discounts.create!(discount_percentage: 20, minimum_quantity: 20, merchant: @merchant1)
+
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@merchant_employee)
   end
 
-  # it "I can edit an existing discount " do
-  #   visit "/merchant/discounts
-  # end
+  it "I can edit a discount" do
+    visit "/merchant/discounts/#{@discount1.id}/edit"
+    discount_percentage = 20
+    minimum_quantity = nil
+    fill_in :discount_percentage, with: discount_percentage
+    fill_in :minimum_quantity, with: minimum_quantity
+
+    click_button 'Edit Discount'
+
+    expect(current_path).to eq("/merchants/discounts/#{@discount1.id}")
+    expect(page).to have_content("Discount: #{discount_percentage}%")
+    expect(page).to have_content("Minimum Quantity: #{minimum_quantity} items")
+  end
 
 end
